6210cc261ee4c19c50e7dac635b28f025a1be3d5
wamb18 <nataliangangira@gmail.com>
2023-07-13 14:42:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -it prints out output using pt
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle");
+	return (0); }
